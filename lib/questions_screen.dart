83e8e72b2462c9543b79cee1659e86c78e7b5f42
sivakrishnareddy.p <sivakrishnareddy.p@presidio.com>
2023-04-21 19:07:43 +0530

    Added Dummy Data, lists, maps - third party package

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/answer_button.dart';
+
+import 'package:flutter_quiz_app/models/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +14,48 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionScreen extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Question Screen");
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  currentQuestion.text,
+                  style: GoogleFonts.lato(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                ...currentQuestion.getShuffledAnswers().map(
+                  (answer) {
+                    return Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: AnswerButton(
+                            answerText: answer, onTap: answerQuestion));
+                  },
+                ),
+              ]),
+        ),
+      ),
+    );
   }
 }
